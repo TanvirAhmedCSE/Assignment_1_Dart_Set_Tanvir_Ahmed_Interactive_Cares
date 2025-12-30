ac17a9026db11ba8382da22d144a8af8a5d64731
TanvirAhmedCSE <141907076+TanvirAhmedCSE@users.noreply.github.com>
2025-12-30 20:51:54 +0600

    Update assignment_1_dart_set_test.dart

--- test/assignment_1_dart_set_test.dart
+++ test/assignment_1_dart_set_test.dart
@@ -1,8 +1,1 @@
-import 'package:assignment_1_dart_set/assignment_1_dart_set.dart';
-import 'package:test/test.dart';
 
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
